0991af22e43af990aa1b6293c3323b42f5baf017
Douglas F Shearer <dougal.s@gmail.com>
2019-04-03 22:58:22 +0100

    Add macvim initializer.

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,7 @@ set -l init_path $config_path/initializers
 source $init_path/fzf_ag.fish
 source $init_path/go.fish
 source $init_path/homebrew.fish
+source $init_path/macvim.fish
 source $init_path/rbenv.fish
 source $init_path/scripts.fish
 
---
+++ fish/initializers/macvim.fish
@@ -0,0 +1,1 @@
+set PATH /Applications/MacVim.app/Contents/bin $PATH
