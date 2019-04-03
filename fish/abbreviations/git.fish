e8f9e805614bbe79470916d6bc36fd5186b78eb4
Douglas F Shearer <dougal.s@gmail.com>
2019-04-03 23:03:27 +0100

    Add a whole bunch of git abbreviations.

--- fish/abbreviations/git.fish
+++ fish/abbreviations/git.fish
@@ -1,11 +1,15 @@
-abbr -a g git
 abbr -a ga git add
+abbr -a gap git add --patch
 abbr -a gb git branch
 abbr -a gc git commit
 abbr -a gca git commit -a
 abbr -a gco git checkout
+abbr -a gcof git checkout -f
+abbr -a gd "git diff"
 abbr -a gds "git diff | subl"
+abbr -a gdm "git diff | mvim -R -"
 abbr -a glog git log --graph --pretty=oneline --abbrev-commit --decorate --
 abbr -a gpom git push origin master
 abbr -a gpp "git pull; and git push"
+abbr -a grm git rm
 abbr -a gst git status
