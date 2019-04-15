fc9cd445fc7d9af981a449830741ebb7edcc43d0
Douglas F Shearer <dougal.s@gmail.com>
2019-04-15 14:56:30 +0100

    Remove gid diff | subl abbreviation.

--- abbreviations/git.fish
+++ abbreviations/git.fish
@@ -6,7 +6,6 @@ abbr -a gca git commit -a
 abbr -a gco git checkout
 abbr -a gcof git checkout -f
 abbr -a gd "git diff"
-abbr -a gds "git diff | subl"
 abbr -a gdm "git diff | mvim -R -"
 abbr -a glog git log --graph --pretty=oneline --abbrev-commit --decorate --
 abbr -a gpom git push origin master
