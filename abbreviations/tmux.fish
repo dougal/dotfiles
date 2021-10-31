ca63b2110835443605ed0dca14d309ab6cec9201
Douglas F Shearer <dougal.s@gmail.com>
2021-10-31 12:10:54 +0000

    Add comment to explain why tmux has a fixssh abbreviation.

--- abbreviations/tmux.fish
+++ abbreviations/tmux.fish
@@ -1,2 +1,6 @@
 abbr -a ta "tmux attach"
+
+# Fix SSH agent environment variable staleness.
+# Once a tmux session has been open for some time, the variable describing where
+# to fid the SSH agent socket becomes stale and prevents usage of SSH.
 abbr -a fixssh "set -gx SSH_AUTH_SOCK (tmux showenv SSH_AUTH_SOCK | awk -F"=" '{print \$2}')"
