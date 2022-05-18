0057fd12cdadf58ec94485e7943a55340e1c57f4
Douglas F Shearer <dougal.s@gmail.com>
2022-05-18 17:54:29 +0100

    Fuzz-find PRs.

--- abbreviations/gh.fish
+++ abbreviations/gh.fish
@@ -1,3 +1,6 @@
-# Create a PR, filling in the details automatically from commits, and opening
+# Create a new PR, filling in the details automatically from commits, and opening
 # the web browser.
-abbr -a ghpr gh pr create -f -w -a @me
+abbr -a prn gh pr create -f -w -a @me
+
+# Checkout a PR, using FZF to select the PR from a list.
+abbr -a pr 'gh pr list | fzf | awk "{print \$1}" | xargs -I "{}" gh pr checkout "{}"'
