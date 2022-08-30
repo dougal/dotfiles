835292fc610f9e06989c0100e9c0e20c08e30876
Douglas F Shearer <dougal.s@gmail.com>
2022-08-30 20:21:13 +0100

    Add ruby-lsp config, commented-out.
    Commented out because of behaviour where it underlines an entire class
    when the class fails some rubocop thing. This makes the class
    unreadable.

--- vim/startup/completion.lua
+++ vim/startup/completion.lua
@@ -175,3 +175,45 @@ require'lspconfig'.terraform_lsp.setup{
   -- Add .tf to default types.
   filetypes = { "terraform", "hcl", "tf" }
 }
+
+-- local lspconfig = require("lspconfig")
+-- local configs = require("lspconfig.configs")
+-- local util = require("lspconfig.util")
+
+-- if not configs.ruby_lsp then
+-- 	local enabled_features = {
+-- 		"documentHighlights",
+-- 		"documentSymbols",
+-- 		"foldingRanges",
+-- 		"selectionRanges",
+-- 		-- "semanticHighlighting",
+-- 		"formatting",
+-- 		"codeActions",
+-- 	}
+
+-- 	configs.ruby_lsp = {
+-- 		default_config = {
+-- 			-- cmd = { "bundle", "exec", "ruby-lsp" },
+-- 			cmd = { "ruby-lsp" },
+-- 			filetypes = { "ruby" },
+-- 			root_dir = util.root_pattern("Gemfile", ".git"),
+-- 			init_options = {
+-- 				enabledFeatures = enabled_features,
+-- 			},
+-- 			settings = {},
+-- 		},
+-- 		commands = {
+-- 			FormatRuby = {
+-- 				function()
+-- 					vim.lsp.buf.format({
+-- 						name = "ruby_lsp",
+-- 						async = true,
+-- 					})
+-- 				end,
+-- 				description = "Format using ruby-lsp",
+-- 			},
+-- 		},
+-- 	}
+-- end
+
+-- lspconfig.ruby_lsp.setup({ on_attach = on_attach, capabilities = capabilities })
