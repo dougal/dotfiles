d1ddf42c0e451b953a53987bc9147e48c27ff010
Douglas F Shearer <dougal.s@gmail.com>
2022-09-17 22:41:48 +0100

    Add LSP config for lua.

--- vim/startup/completion.lua
+++ vim/startup/completion.lua
@@ -194,3 +194,26 @@ require'lspconfig'.terraform_lsp.setup{
 -- end
 
 -- lspconfig.ruby_lsp.setup({ on_attach = on_attach, capabilities = capabilities })
+
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
