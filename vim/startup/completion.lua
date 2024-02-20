362bca4f54d7b28d6466077dd7bfed2274f717c0
Douglas F Shearer <dougal.s@gmail.com>
2024-02-20 21:18:41 +0000

    Use lsp-zero to setup LSP interaction

--- vim/startup/completion.lua
+++ vim/startup/completion.lua
@@ -1,84 +1,135 @@
-vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, {})
+local lsp = require("lsp-zero")
 
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
+-- Fix Undefined global 'vim'
+-- lsp.nvim_workspace()
 
-local lspkind = require "lspkind"
-lspkind.init()
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
 
-local cmp = require "cmp"
+-- cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<S-Tab>'] = nil
 
-local select_opts = {behaviour = cmp.SelectBehavior.Select}
+-- lsp.setup_nvim_cmp({
+--   mapping = cmp_mappings
+-- })
 
-cmp.setup {
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<c-y>"] = cmp.mapping(
-      cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-      { "i", "c" }
-    ),
-  },
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
 
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "buffer", keyword_length = 3 },
-    { name = "tags" },
-    { name = "path" },
-    -- { name = "luasnip" },
-  },
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
 
-  formatting = {
-    format = lspkind.cmp_format {
-      mode = "text",
-      menu = {
-        buffer = "[buf]",
-        nvim_lsp = "[LSP]",
-        path = "[path]",
-        tags = "[tags]",
-      },
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>fm", function() vim.lsp.buf.format() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
 
-      -- Disable symbols.
-      symbol_map = {
-        Class         = '',
-        Color         = '',
-        Constant      = '',
-        Constructor   = '',
-        Enum          = '',
-        EnumMember    = '',
-        Event         = '',
-        Field         = '',
-        File          = '',
-        Folder        = '',
-        Function      = '',
-        Interface     = '',
-        Keyword       = '',
-        Method        = '',
-        Module        = '',
-        Operator      = '',
-        Property      = '',
-        Reference     = '',
-        Snippet       = '',
-        Struct        = '',
-        Text          = '',
-        TypeParameter = '',
-        Unit          = '',
-        Variable      = '',
-      }
-    },
-  },
+lsp.setup()
 
-  experimental = {
-    -- native_menu = false,
+vim.diagnostic.config({
+    virtual_text = true
+})
 
-    ghost_text = false,
-  },
-}
+-- vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- local lspkind = require "lspkind"
+-- lspkind.init()
+
+-- local cmp = require "cmp"
+
+-- local select_opts = {behaviour = cmp.SelectBehavior.Select}
+
+-- cmp.setup {
+--   mapping = {
+--     ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+--     ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+--     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+--     ["<C-f>"] = cmp.mapping.scroll_docs(4),
+--     ["<C-e>"] = cmp.mapping.close(),
+--     ["<c-y>"] = cmp.mapping(
+--       cmp.mapping.confirm {
+--         behavior = cmp.ConfirmBehavior.Insert,
+--         select = true,
+--       },
+--       { "i", "c" }
+--     ),
+--   },
+
+--   sources = {
+--     { name = "nvim_lsp" },
+--     { name = "buffer", keyword_length = 3 },
+--     { name = "tags" },
+--     { name = "path" },
+--     -- { name = "luasnip" },
+--   },
+
+--   formatting = {
+--     format = lspkind.cmp_format {
+--       mode = "text",
+--       menu = {
+--         buffer = "[buf]",
+--         nvim_lsp = "[LSP]",
+--         path = "[path]",
+--         tags = "[tags]",
+--       },
+
+--       -- Disable symbols.
+--       symbol_map = {
+--         Class         = '',
+--         Color         = '',
+--         Constant      = '',
+--         Constructor   = '',
+--         Enum          = '',
+--         EnumMember    = '',
+--         Event         = '',
+--         Field         = '',
+--         File          = '',
+--         Folder        = '',
+--         Function      = '',
+--         Interface     = '',
+--         Keyword       = '',
+--         Method        = '',
+--         Module        = '',
+--         Operator      = '',
+--         Property      = '',
+--         Reference     = '',
+--         Snippet       = '',
+--         Struct        = '',
+--         Text          = '',
+--         TypeParameter = '',
+--         Unit          = '',
+--         Variable      = '',
+--       }
+--     },
+--   },
+
+--   experimental = {
+--     -- native_menu = false,
+
+--     ghost_text = false,
+--   },
+-- }
 
 cmp.setup.cmdline("/", {
   completion = {
