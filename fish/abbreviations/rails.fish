7de07d0d267e2d81d60f993ac458d42273f27ecb
Douglas F Shearer <d.shearer@medify.co.uk>
2024-03-07 17:17:49 +0000

    No longer do prepare as part of DB migrate

--- fish/abbreviations/rails.fish
+++ fish/abbreviations/rails.fish
@@ -2,5 +2,5 @@ abbr -a r bin/rails
 abbr -a rc bin/rails console
 abbr -a rdb bin/rails dbconsole -p
 abbr -a rt bin/rails test
-abbr -a rdm "bin/rails db:migrate db:test:prepare"
+abbr -a rdm "bin/rails db:migrate"
 abbr -a cpd bundle exec cap production deploy
