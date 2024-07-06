035ac4d97560b2d896e344b94d9ae6f0e0f992fd
Douglas F Shearer <dougal.s@gmail.com>
2024-07-06 13:30:57 +0100

    Add targetted tf destroy and apply using FZF

--- fish/abbreviations/terraform.fish
+++ fish/abbreviations/terraform.fish
@@ -2,7 +2,11 @@
 abbr -a tff tf fmt
 abbr -a tfi tf init
 abbr -a tfp tf plan
+
 abbr -a tfa tf apply
+abbr -a tfat 'tf apply --target=$(tf state list | fzf)'
 abbr -a tfay tf apply -auto-approve
+
 abbr -a tfd tf destroy
+abbr -a tfdt 'tf destroy --target=$(tf state list | fzf)'
 
