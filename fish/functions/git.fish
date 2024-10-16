d3989e557f6760807c9d5b897ebf03f7078080a1
Douglas F Shearer <d.shearer@medify.co.uk>
2024-10-16 11:50:38 +0100

    Remove bare-words commit functions as wasn't a fan.

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -4,13 +4,3 @@ function gnb
   set branchname (echo $argv | tr '[:upper:]' '[:lower:]' | sed -E 's/[^a-z0-9\.]+/-/g')
   git switch -c $branchname
 end
-
-# Create a new commit from bare words.
-function gcm
-  git commit -m "$argv"
-end
-
-# Add all tracked files and create a new commit from bare words.
-function gcam
-  git commit -a -m "$argv"
-end
