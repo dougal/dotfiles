4ae8ffad3e68e588156c9b10da8baca7125d5437
Douglas F Shearer <dougal.s@gmail.com>
2026-07-18 17:12:10 +0100

    All-tofu, all the time

--- fish/tools/terraform.fish
+++ fish/tools/terraform.fish
@@ -1,26 +1,14 @@
 set -gx TF_PLUGIN_CACHE_DIR "$HOME/.terraform.d/plugin-cache"
 
-# Runs whichever of terraform or opentofu is installed
-function tf
-
-	# Is terraform installed?
-  if test -n "$(which terraform)"
-		terraform $argv
-	else if test -n "$(which tofu)"
-		tofu $argv
-  else
-    echo "No terraform or opentofu installed"
-  end
-end
-
 #Makes use of the `tf` function to be Terraform and OpenTofu agnostic.
-abbr -a tff tf fmt
-abbr -a tfi tf init
-abbr -a tfp tf plan
+abbr -a tf tofu
+abbr -a tff tofu fmt
+abbr -a tfi tofu init
+abbr -a tfp tofu plan
 
-abbr -a tfa tf apply
-abbr -a tfat 'tf apply --target=$(tf state list | fzf)'
-abbr -a tfay tf apply -auto-approve
+abbr -a tfa tofu apply
+abbr -a tfat 'tofu apply --target=$(tofu state list | fzf)'
+abbr -a tfay tofu apply -auto-approve
 
-abbr -a tfd tf destroy
-abbr -a tfdt 'tf destroy --target=$(tf state list | fzf)'
+abbr -a tfd tofu destroy
+abbr -a tfdt 'tofu destroy --target=$(tofu state list | fzf)'
